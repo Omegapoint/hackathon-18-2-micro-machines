a4921d0d2d45ea1a935d44cd30a8086a18fbdbc7
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-10-12 20:33:19 +0200

    Implement board

--- client/static/js/render.js
+++ client/static/js/render.js
@@ -4,20 +4,35 @@ let ctx
 function start() {
   canvas = document.getElementById('canvas')
   ctx = canvas.getContext('2d')
-  setInterval(renderCars, 50)
+  setInterval(paintWorld, 50)
 }
 
 
-function renderCars () {
+function paintWorld() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  renderCars()
+  renderObjects()
+}
+
+function renderCars () {
   drawCar(otherCars[0])
   if (myCar.loaded) {
     drawCar(myCar)
   }
 }
 
+function renderObjects() {
+  board.getObjects().forEach(obj => {
+    drawSquare(obj.x, obj.y)
+  })
+}
+
 function drawCar(car) {
   drawImage(car.image, car.x, car.y, car.rotation)
+}
+
+function drawSquare(x, y) {
+  ctx.fillRect(x, y, 10, 10)
 }
 
 function drawImage(image, x, y, rotation) {
